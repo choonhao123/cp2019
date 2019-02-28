16678a470288ce49ab0514634883f3c904259de1
choonhao123 <ng.choonhao@dhs.sg>
2019-02-28 12:00:36 +0800

    Update q1_display_reverse.py

--- p03/q1_display_reverse.py
+++ p03/q1_display_reverse.py
@@ -1,8 +1,12 @@
-Number = int(input("Please Enter any Number: "))    
-Reverse = 0    
-while(Number > 0):    
-    Reminder = Number %10    
-    Reverse = (Reverse *10) + Reminder    
-    Number = Number //10    
+num = int(input("Please Enter any Number: "))        
+
+def reverse_int(n):
+    Number = num
+    Reverse = 0
+    while(Number > 0):    
+        Reminder = Number %10    
+        Reverse = (Reverse *10) + Reminder    
+        Number = Number //10
+    return Reverse
     
-print(Reverse)
+print(reverse_int(num))
