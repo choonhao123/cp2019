df3eb6b2196676b45ac34ab60009449e7604d5a8
choonhao123 <ng.choonhao@dhs.sg>
2019-03-02 15:35:28 +0800

    Create q6_determine_prime.py

---
+++ p03/q6_determine_prime.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n==2 or n==3:
+        return True
+    for i in range(2, n-1):
+        if n%i==0:
+            return False
+        else:
+            return True
+        
+q = 0
+x = 1
+while q<1000:
+    if is_prime(x)==True:
+        print(x,end=" ")
+        q += 1
+        if q%10==0:
+            print()
+    x += 1
