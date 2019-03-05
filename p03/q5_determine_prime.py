d26d72dcd3405629076a246f40270c97f4138643
choonhao123 <ng.choonhao@dhs.sg>
2019-03-05 11:02:27 +0800

    Update q5_determine_prime.py

--- p03/q5_determine_prime.py
+++ p03/q5_determine_prime.py
@@ -2,7 +2,7 @@ def is_prime(n):
     if n==2 or n==3:
         return True
     for i in range(2, n-1):
-        if n%i==0:
+        if n%2==0 or n%3==0 or n%5==0:
             return False
         else:
             return True
